839f22eb169cd44ff7ba12dfa8ce7f7dc3cc90a5
leonardo185 <leonardo18598@gmail.com>
2019-05-07 19:20:20 +0530

    first commit

---
+++ mysite/hello/views.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello World!")
+
+def second(request):
+    return HttpResponse("Second page")    
