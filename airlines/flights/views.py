70b17c62ec677f1b5f727f03173bd6255efa5f40
leonardo185 <leonardo@DESKTOP-BQBDUCC.localdomain>
2019-05-24 06:24:50 +0530

    The new commit

--- airlines/flights/views.py
+++ airlines/flights/views.py
@@ -1,14 +1,3 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
-from django.http import HttpResponse
 
-from .models import Flight
 # Create your views here.
-def index(request):
-    context ={
-        "flights": Flight.objects.all()
-    }
-
-    return render(request, "flights/index.html", context)
